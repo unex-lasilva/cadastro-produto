250f4f4ba2e59216c4ca1acb11c4488a3a9775c7
Arthur De Aquino <Sairth0@gmail.com>
2025-05-22 21:27:43 -0300

    initial commit

--- cadastros/app/src/main/java/com/example/cadastros/CadastroProdutoScreen.kt
+++ cadastros/app/src/main/java/com/example/cadastros/CadastroProdutoScreen.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -21,6 +23,15 @@ fun CadastroProdutoScreen() {
     var precoCusto by remember { mutableStateOf("") }
     var precoVenda by remember { mutableStateOf("") }
     var marca by remember { mutableStateOf("") }
+
+    val isNomeValido = nomeProduto.length >= 3
+    val isQuantidadeValida = quantidade.toIntOrNull()?.let { it > 0 } == true
+    val isPrecoCustoValido = precoCusto.toDoubleOrNull()?.let { it > 0 } == true
+    val isPrecoVendaValido = precoVenda.toDoubleOrNull()?.let { it > 0 } == true
+    var formSubmited by remember { mutableStateOf(false) }
+    var enviou by remember { mutableStateOf(false) }
+    var produto by remember { mutableStateOf<Produto?>(null) }
+
 
     Column(
         modifier = Modifier
@@ -40,35 +51,72 @@ fun CadastroProdutoScreen() {
         // Nome do produto
         OutlinedTextField(
             value = nomeProduto,
-            onValueChange = { nomeProduto = it },
+            onValueChange = {
+                    nomeProduto = it
+
+            },
             label = { Text("Nome do produto") },
             keyboardOptions = KeyboardOptions.Default,
             modifier = Modifier.fillMaxWidth()
         )
+
+        if ((nomeProduto.length < 3) && (formSubmited || nomeProduto.isNotEmpty())) {            Text(
+                text = "O nome do produto deve ter pelo menos 3 caracteres",
+                color = Color.Red,
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
 
         Spacer(modifier = Modifier.height(16.dp))
 
         // Quantidade em estoque
         OutlinedTextField(
             value = quantidade,
-            onValueChange = { quantidade = it },
+            onValueChange = {
+                    quantidade = it
+
+            },
             label = { Text("Quantidade em estoque") },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             modifier = Modifier.fillMaxWidth()
         )
+
+        val quantidadeint = quantidade.toIntOrNull();
+
+        if ((quantidadeint == null || quantidadeint < 1) && (formSubmited || quantidade.isNotEmpty())) {
+            Text(
+                text = "Deve haver pelo menos 1 produto em estoque",
+                color = Color.Red,
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
 
         Spacer(modifier = Modifier.height(16.dp))
 
         // Preço de custo
         OutlinedTextField(
             value = precoCusto,
-            onValueChange = { precoCusto = it },
+            onValueChange = {
+                    precoCusto = it
+
+            },
             label = { Text("Preço de custo") },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             modifier = Modifier.fillMaxWidth()
         )
 
+        val precoCustoDoub = precoCusto.toDoubleOrNull()
+
+        if (precoCusto.isNotEmpty() && (precoCustoDoub == null || precoCustoDoub <= 0)) {
+                Text(
+                    text = "O produto deve ter um custo maior que zero",
+                    color = Color.Red,
+                    style = MaterialTheme.typography.bodySmall
+                )
+        }
+
         Spacer(modifier = Modifier.height(16.dp))
+
 
         // Preço de venda
         OutlinedTextField(
@@ -79,7 +127,18 @@ fun CadastroProdutoScreen() {
             modifier = Modifier.fillMaxWidth()
         )
 
+        val precoVendaDoub = precoVenda.toDoubleOrNull()
+
+        if ((precoVendaDoub == null || precoVendaDoub == 0.0) && (formSubmited || precoVenda.isNotEmpty())) {
+            Text(
+                text = "O preço de venda deve ser maior que zero",
+                color = Color.Red,
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+
         Spacer(modifier = Modifier.height(16.dp))
+
 
         // Nome da marca
         OutlinedTextField(
@@ -98,19 +157,47 @@ fun CadastroProdutoScreen() {
             modifier = Modifier.fillMaxWidth()
         ) {
             Button(onClick = {
-                // Ação de salvar
+                formSubmited = true
+                if (isNomeValido && isQuantidadeValida && isPrecoCustoValido && isPrecoVendaValido) {
+                    enviou = true
+                    produto = Produto(nomeProduto, quantidadeint, precoCustoDoub, precoVendaDoub, marca)
+
+                }
             }) {
                 Text("Salvar")
             }
 
             OutlinedButton(onClick = {
-                // Ação de cancelar
+                nomeProduto = ""
+                quantidade = ""
+                precoCusto = ""
+                precoVenda = ""
+                marca = ""
+                formSubmited = false
+                enviou = false
             }) {
                 Text("Cancelar")
             }
         }
+
+        if (enviou) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Produto salvo com sucesso!",
+                    color = Color.Green,
+                    fontSize = 18.sp,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
---
+++ cadastros/app/src/main/java/com/example/cadastros/Produto.kt
@@ -0,0 +1,12 @@
+package com.example.cadastros
+
+class Produto (
+    var nome: String,
+    var quantidade: Int?,
+    var custo: Double?,
+    var preco: Double?,
+    var marca:String = ""
+
+){
+
+}
