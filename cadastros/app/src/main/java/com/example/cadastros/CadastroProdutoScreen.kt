c75c2890caa8171f0449fec25618078b29df2169
Denilson Santos <144867249+DenilsonSantos-OFC@users.noreply.github.com>
2025-05-22 21:44:32 -0300

    Update CadastroProdutoScreen.kt

--- cadastros/app/src/main/java/com/example/cadastros/CadastroProdutoScreen.kt
+++ cadastros/app/src/main/java/com/example/cadastros/CadastroProdutoScreen.kt
@@ -1,17 +1,33 @@
 package com.example.cadastros
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.os.unregisterForAllProfilingResults
+import com.example.cadastros.classes.Produto
+import java.math.BigDecimal
 
 @Composable
 fun CadastroProdutoScreen() {
@@ -22,11 +38,14 @@ fun CadastroProdutoScreen() {
     var precoVenda by remember { mutableStateOf("") }
     var marca by remember { mutableStateOf("") }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
+    var nomeProdutoErro by remember { mutableStateOf("") }
+    var quantidadeErro by remember { mutableStateOf("") }
+    var precoCustoErro by remember { mutableStateOf("") }
+    var precoVendaErro by remember { mutableStateOf("") }
+
+    var cadastroStatus by remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
         // Título
         Text(
             text = "Cadastro de produto",
@@ -37,57 +56,88 @@ fun CadastroProdutoScreen() {
 
         Spacer(modifier = Modifier.height(24.dp))
 
-        // Nome do produto
-        OutlinedTextField(
-            value = nomeProduto,
-            onValueChange = { nomeProduto = it },
-            label = { Text("Nome do produto") },
-            keyboardOptions = KeyboardOptions.Default,
-            modifier = Modifier.fillMaxWidth()
+        // Nome do Produto
+        Field(
+            textLabel = "Nome do Produto",
+            textValue = nomeProduto,
+            valueIsOk = nomeProdutoErro.isBlank(),
+            onValueChange = {
+                nomeProduto = it
+                val erros = ErrorGetter(nomeProduto, quantidade, precoCusto, precoVenda)
+                nomeProdutoErro = erros[0]
+                quantidadeErro = erros[1]
+                precoCustoErro = erros[2]
+                precoVendaErro = erros[3]
+            }
         )
+        Text(text = nomeProdutoErro, color = Color.Red)
 
         Spacer(modifier = Modifier.height(16.dp))
+
 
         // Quantidade em estoque
-        OutlinedTextField(
-            value = quantidade,
-            onValueChange = { quantidade = it },
-            label = { Text("Quantidade em estoque") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier.fillMaxWidth()
+        Field(
+            textLabel = "Quantidade em estoque",
+            textValue = quantidade,
+            keyboardType = KeyboardType.Number,
+            onValueChange = {
+                quantidade = it
+                val erros = ErrorGetter(nomeProduto, quantidade, precoCusto, precoVenda)
+                nomeProdutoErro = erros[0]
+                quantidadeErro = erros[1]
+                precoCustoErro = erros[2]
+                precoVendaErro = erros[3]
+            },
+            valueIsOk = quantidadeErro.isBlank()
         )
+        Text(text = quantidadeErro, color = Color.Red)
 
         Spacer(modifier = Modifier.height(16.dp))
 
+
         // Preço de custo
-        OutlinedTextField(
-            value = precoCusto,
-            onValueChange = { precoCusto = it },
-            label = { Text("Preço de custo") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier.fillMaxWidth()
+        Field(
+            textLabel = "Preço de custo",
+            textValue = precoCusto,
+            keyboardType = KeyboardType.Decimal,
+            onValueChange = {
+                precoCusto = it
+                val erros = ErrorGetter(nomeProduto, quantidade, precoCusto, precoVenda)
+                nomeProdutoErro = erros[0]
+                quantidadeErro = erros[1]
+                precoCustoErro = erros[2]
+                precoVendaErro = erros[3]
+            },
+            valueIsOk = precoCustoErro.isBlank()
         )
+        Text(text = precoCustoErro, color = Color.Red)
 
         Spacer(modifier = Modifier.height(16.dp))
 
         // Preço de venda
-        OutlinedTextField(
-            value = precoVenda,
-            onValueChange = { precoVenda = it },
-            label = { Text("Preço de venda") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier.fillMaxWidth()
+        Field(
+            textLabel = "Preço de venda",
+            textValue = precoVenda,
+            keyboardType = KeyboardType.Decimal,
+            onValueChange = {
+                precoVenda = it
+                val erros = ErrorGetter(nomeProduto, quantidade, precoCusto, precoVenda)
+                nomeProdutoErro = erros[0]
+                quantidadeErro = erros[1]
+                precoCustoErro = erros[2]
+                precoVendaErro = erros[3]
+            },
+            valueIsOk = precoVendaErro.isBlank()
         )
+        Text(text = precoVendaErro, color = Color.Red)
 
         Spacer(modifier = Modifier.height(16.dp))
 
         // Nome da marca
-        OutlinedTextField(
-            value = marca,
+        Field(
+            textLabel = "Nome da marca",
+            textValue = marca,
             onValueChange = { marca = it },
-            label = { Text("Nome da marca") },
-            keyboardOptions = KeyboardOptions.Default,
-            modifier = Modifier.fillMaxWidth()
         )
 
         Spacer(modifier = Modifier.height(32.dp))
@@ -98,7 +148,12 @@ fun CadastroProdutoScreen() {
             modifier = Modifier.fillMaxWidth()
         ) {
             Button(onClick = {
-                // Ação de salvar
+                cadastroStatus = ErrorGetter(nomeProduto, quantidade, precoCusto, precoVenda).joinToString("")
+                if (cadastroStatus.isBlank()) {
+                    cadastroStatus = "OK!"
+                } else {
+                    cadastroStatus = "Preencha os campos necessários e corrija os erros existentes!"
+                }
             }) {
                 Text("Salvar")
             }
@@ -109,11 +164,101 @@ fun CadastroProdutoScreen() {
                 Text("Cancelar")
             }
         }
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+
+        Text(text = cadastroStatus, color = if (cadastroStatus == "OK!") Color.Green else Color.Red)
     }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun CadastroProdutoPreview() {
     CadastroProdutoScreen()
+}
+
+@Composable
+fun Field(
+    modifier: Modifier = Modifier,
+    textLabel: String,
+    textValue: String,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    onValueChange: (String) -> Unit,
+    valueIsOk: Boolean = true
+) {
+    val color = when {
+        !valueIsOk -> Color.Red
+        textValue.isNotBlank() -> Color(0xFF0E3400)
+        else -> Color.Gray
+    }
+
+    OutlinedTextField(
+        value = textValue,
+        onValueChange = { onValueChange(it) },
+        label = { Text(textLabel) },
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+        modifier = modifier,
+        colors = OutlinedTextFieldDefaults.colors(
+            unfocusedBorderColor = color.copy(alpha = 0.7f),
+            unfocusedLabelColor = color.copy(alpha = 0.7f),
+            focusedBorderColor = color,
+            focusedLabelColor = color
+        )
+    )
+}
+
+
+object ErrorGetter {
+
+    operator fun invoke(
+        nomeProduto: String,
+        qntProduto: String,
+        precoCusto: String,
+        precoVenda: String
+    ): Array<String> {
+        return arrayOf(
+            ErrorGetter.nomeProduto(nomeProduto),
+            ErrorGetter.qntEstoque(qntProduto),
+            ErrorGetter.precoCusto(precoCusto),
+            ErrorGetter.precoVenda(precoVenda)
+        )
+    }
+
+    fun nomeProduto(nomeProduto: String): String {
+        return if (nomeProduto.length < 3) {
+            "Nome do produto tem menos de 3 caracteres"
+        } else {
+            ""
+        }
+    }
+
+    fun qntEstoque(qntEstoque: String): String {
+        val qntEstoqueInt = qntEstoque.toIntOrNull()
+        return if (qntEstoqueInt == null || qntEstoqueInt <= 0) {
+            "Quantidade em estoque inválida"
+        } else {
+            ""
+        }
+    }
+
+    fun precoCusto(precoCusto: String): String {
+        val precoCustoBigDecimal = precoCusto.toBigDecimalOrNull()
+        return if (precoCustoBigDecimal != null && precoCustoBigDecimal <= BigDecimal(0)) {
+            "Preço de custo inválido"
+        } else {
+            ""
+        }
+    }
+
+    fun precoVenda(precoVenda: String): String {
+        val precoVendaBigDecimal = precoVenda.toBigDecimalOrNull()
+        return if (precoVendaBigDecimal != null && precoVendaBigDecimal <= BigDecimal(0)) {
+            "Preço de venda inválido"
+        } else {
+            ""
+        }
+    }
 }
